1e58a7e626edbee96b48206733bdbd578235e30d
Nithin Muthukumar <kathrikai30201094@gmail.com>
2024-04-06 21:02:40 -0400

    adding basic info

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # shrs_sound
+
+Sound plugin for shrs
+
+This lib provides a Plugin that allows users to easily play sounds in hooks.
+Sounds should be stored in config_dir/sounds
+
+see examples/basic.rs
--- examples/basic.rs
+++ examples/basic.rs
@@ -16,21 +16,6 @@ pub fn command_finish_sfx(
     Ok(())
 }
 
-pub fn switch_mode_sfx(
-    _sh: &Shell,
-    _ctx: &mut Context,
-    _rt: &mut Runtime,
-    _lms_ctx: &LineModeSwitchCtx,
-) -> anyhow::Result<()> {
-    //     if let Some(stream) = ctx.state.get::<AudioStreamState>() {
-    //         match lms_ctx.line_mode {
-    //             shrs::readline::LineMode::Insert => stream.play_sound("on.wav", 0.5)?,
-    //             shrs::readline::LineMode::Normal => stream.play_sound("off.wav", 0.5)?,
-    //         };
-    //     }
-    //
-    Ok(())
-}
 pub fn startup_sfx(
     _sh: &Shell,
     ctx: &mut Context,
